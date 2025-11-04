4892248ee97f26ce0454076e5324edd33f0d1b71
chandrika131105 <chandrika131105@gmail.com>
2025-11-04 21:58:42 +0530

    feat: add Book model

---
+++ src/main/java/librarymanager/Book.java
@@ -0,0 +1,39 @@
+package librarymanager;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // Convenience constructor when ISBN is not provided
+    public Book(String title, String author) {
+        this(title, author, "");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    @Override
+    public String toString() {
+    return "Book{" +
+        "title='" + title + '\'' +
+        ", author='" + author + '\'' +
+        ", isbn='" + isbn + '\'' +
+        '}';
+    }
+}
